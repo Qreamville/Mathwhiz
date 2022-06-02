c5fb21da026db2611d56603feb46507cb7ab8291
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-02 00:23:11 -1200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { Header } from "./components";
+import data from "./assets/data.json";
 
 function App() {
   const [theme, setTheme] = useState(false);
   return (
-    <div className={`App ${theme ? "dark" : ""}`}>
+    <div className={`App${theme ? "dark" : ""}`}>
       <div className="main dark:bg-slate-900">
         <Header theme={theme} setTheme={setTheme} />
       </div>
