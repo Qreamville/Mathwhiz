888642524a5259a6cd30bc1add6715e9f1042eac
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-02 05:30:53 -1200

    Add score state

--- src/context/Context.js
+++ src/context/Context.js
@@ -3,9 +3,12 @@ import React, { createContext, useContext, useState } from "react";
 const QuizContext = createContext();
 const AppContext = ({ children }) => {
   const [questions, setQuestions] = useState(null);
+  const [score, setScore] = useState(0);
 
   return (
-    <QuizContext.Provider value={{ questions, setQuestions }}>
+    <QuizContext.Provider
+      value={{ questions, setQuestions, score, setQuestions }}
+    >
       {children}
     </QuizContext.Provider>
   );
