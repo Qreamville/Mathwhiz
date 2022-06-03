0b591af96521e8659bd920c1c8275ace9a4139d1
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-03 02:36:40 -1200

    Update header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { FaSun, FaMoon } from "react-icons/fa";
 
 const Header = ({ theme, setTheme }) => {
   return (
-    <header className="border-b p-2 sm:py-2  dark:border-gray-600">
+    <header className="border-b py-2  dark:border-gray-600">
       <div className="container mx-auto flex justify-between items-center w-full">
         <h1 className="text-slate-900 dark:text-white text-3xl font-bold">
           Math<span className="text-orange-600">whiz</span>
